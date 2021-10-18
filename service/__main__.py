dff759a4bcbf33cb28514282d45651317f3dd77e
Evgenij Karpov <gedrip87@gmail.com>
2021-10-18 05:06:23 +0300

    update pyfiles after using linters

--- service/__main__.py
+++ service/__main__.py
@@ -3,11 +3,13 @@ from service.dostoevsky_recognizer import dostoevsky
 
 app = Flask(__name__)
 
-@app.route("/api/v1/predict", methods = ['POST'])
+
+@app.route("/api/v1/predict", methods=['POST'])
 def predict_slow():
     data = request.json
     emotion = dostoevsky(data['text'])
     return {'emotions': [emotion]}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
